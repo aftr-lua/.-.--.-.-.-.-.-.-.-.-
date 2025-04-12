3ed95591e7c4c13c8a3207462e9c11b9487b27de
aftr-lua <154597947+aftr-lua@users.noreply.github.com>
2025-04-12 14:14:07 +0700

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,31 +1,27 @@
--- toggle from scriptblox
-repeat task.wait(0.25) until game:IsLoaded();
+
 getgenv().Image = "rbxassetid://15915524325"; -- put a asset id in here to make it work
 getgenv().ToggleUI = "E" -- This where you can Toggle the Fluent ui library
 
-task.spawn(function()
-    if not getgenv().LoadedMobileUI == true then getgenv().LoadedMobileUI = true
-        local OpenUI = Instance.new("ScreenGui");
-        local ImageButton = Instance.new("ImageButton");
-        local UICorner = Instance.new("UICorner");
-        OpenUI.Name = "OpenUI";
-        OpenUI.Parent = game:GetService("CoreGui");
-        OpenUI.ZIndexBehavior = Enum.ZIndexBehavior.Sibling;
-        ImageButton.Parent = OpenUI;
-        ImageButton.BackgroundColor3 = Color3.fromRGB(105,105,105);
-        ImageButton.BackgroundTransparency = 0.8
-        ImageButton.Position = UDim2.new(0.9,0,0.1,0);
-        ImageButton.Size = UDim2.new(0,50,0,50);
-        ImageButton.Image = getgenv().Image;
-        ImageButton.Draggable = true;
-        ImageButton.Transparency = 1;
-        UICorner.CornerRadius = UDim.new(0,200);
-        UICorner.Parent = ImageButton;
-        ImageButton.MouseButton1Click:Connect(function()
-            game:GetService("VirtualInputManager"):SendKeyEvent(true,getgenv().ToggleUI,false,game);
-        end)
-    end
+local OpenUI = Instance.new("ScreenGui");
+local ImageButton = Instance.new("ImageButton");
+local UICorner = Instance.new("UICorner");
+OpenUI.Name = "OpenUI";
+OpenUI.Parent = game:GetService("CoreGui");
+OpenUI.ZIndexBehavior = Enum.ZIndexBehavior.Sibling;
+ImageButton.Parent = OpenUI;
+ImageButton.BackgroundColor3 = Color3.fromRGB(105,105,105);
+ImageButton.BackgroundTransparency = 0.8
+ImageButton.Position = UDim2.new(0.9,0,0.1,0);
+ImageButton.Size = UDim2.new(0,50,0,50);
+ImageButton.Image = getgenv().Image;
+ImageButton.Draggable = true;
+ImageButton.Transparency = 1;
+UICorner.CornerRadius = UDim.new(0,200);
+UICorner.Parent = ImageButton;
+ImageButton.MouseButton1Click:Connect(function()
+        game:GetService("VirtualInputManager"):SendKeyEvent(true,getgenv().ToggleUI,false,game);
 end)
+
 
 local Fluent = loadstring(game:HttpGet("https://raw.githubusercontent.com/AntiOderHubLtd/ThereIsASecretRepoButYouCantSkid/refs/heads/main/UILib.lua"))()
 local SaveManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/AntiOderHubLtd/ThereIsASecretRepoButYouCantSkid/refs/heads/main/SaveManager.lua"))()
